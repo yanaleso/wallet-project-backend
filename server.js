ec0bc5345fbe635c89895f3f17edfc989d4b9052
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-11-19 09:54:59 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const app = require('./app')
+const { DB_HOST, PORT = 3000 } = process.env;
+console.log(DB_HOST);
 
-const {DB_HOST, PORT = 3000} = process.env
-
-mongoose.connect(DB_HOST)
-  .then(() => app.listen(PORT, () => {
-    console.log("Database connection successful")
-  }))
-  .catch(error => {
-    console.log(error.message)
-    process.exit(1)
-  })
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		console.log("Database connection successful");
+		app.listen(PORT);
+	})
+	.catch((error) => {
+		console.log(error.message);
+		process.exit(1);
+	});
