4431b634fcc7563dbc9a6bbc52e31dcf8b7646df
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-11-22 19:36:58 +0200

    commit stat.

---
+++ controllers/transactions/getStatistics.js
@@ -0,0 +1,22 @@
+const { Transaction } = require("../../models/transaction");
+const { ObjectId } = require("mongoose").Types;
+
+const getStatistics = async (req, res, next) => {
+	try {
+		const _id = req.userId;
+		const objID = `ObjectId('${_id}')`;
+		const { month = null, year = null } = req.query;
+		if (month && !year) year = "2022";
+		console.log("id:", ObjectId(_id));
+
+		const statistics = await Transaction.aggregate().match({
+			owner: ObjectId(_id),
+			// year: { $lt: "2022" },
+		});
+		res.json(statistics);
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = getStatistics;
