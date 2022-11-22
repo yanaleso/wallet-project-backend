de71d537726dc8dd9c216669f8b42f6bf1205860
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-11-22 23:17:47 +0200

    feat: statistic aggregation

--- controllers/transactions/add.js
+++ controllers/transactions/add.js
@@ -4,9 +4,14 @@ const add = async (req, res, next) => {
 	try {
 		const { error } = addSchema.validate(req.body);
 		if (error) throw new Error(error);
+		const timestamps = Number(new Date(req.body.date).getTime());
+		if (req.body.typeOperation === "expense") {
+			req.body.category = "Regular Income";
+		}
 		const transaction = await Transaction.create({
 			...req.body,
 			owner: req.userId,
+			timestamps,
 		});
 		res.status(201).json(transaction);
 	} catch (error) {
