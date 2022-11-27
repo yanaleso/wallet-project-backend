91480dd4614b978f24312ac6fcb31865d6489712
Krasnonos <andreykrasnonos@icloud.com>
2022-11-26 21:09:36 -0500

    maybe fix auth service

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -1,8 +1,19 @@
 const { User } = require("../../models/user");
+const { createError } = require("../../helpers/createError");
 
 const current = async (req, res, next) => {
-  const { email } = req.user;
-  res.json({ email });
+  const { _id  } = req.user;
+  const user = await User.findById(_id);
+  
+  if (!user) {
+    throw createError(401, "Token is required");
+  }
+
+  res.status(200).json({
+    email: user.email,
+    firstName: user.firstName,
+    balance: user.balance
+  });
 };
 
 module.exports = current;
